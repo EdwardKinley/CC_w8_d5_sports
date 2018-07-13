0c4c0165d707095a26e4d326f34558169518a512
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-13 11:52:30 +0100

    sets up Manager class

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,4 +1,47 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "managers")
 public class Manager {
+
+    private int id;
+    private String name;
+    private Team team;
+
+    public Manager() {}
+
+    public Manager(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    public Team getTeam() {
+        return team;
+    }
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+
 }
