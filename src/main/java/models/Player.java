71e656a185537b767280661f35fcf5a605e835c7
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-13 11:57:27 +0100

    sets up Player class

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,4 +1,47 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "players")
 public class Player {
+
+    private int id;
+    private String name;
+    private Team team;
+
+    public Player() {}
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    public Team getTeam() {
+        return team;
+    }
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+
 }
